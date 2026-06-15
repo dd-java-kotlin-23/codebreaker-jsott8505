05aee5dea0d475fc3d0070c182e6b91552f4f65b
jazz <jscott85@student.cnm.edu>
2026-06-15 10:45:04 -0600

    Defining CodebreakerService in progress.

--- services/src/main/java/edu/cnm/deepdive/codebreaker/model/Game.java
+++ services/src/main/java/edu/cnm/deepdive/codebreaker/model/Game.java
@@ -2,18 +2,17 @@ package edu.cnm.deepdive.codebreaker.model;
 
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.function.Predicate;
 
 // TODO: 6/15/26 Add guesses parameter after defining the Guess record. 
 public record Game(String id, String pool, int length, List<Guess> guesses,
                    OffsetDateTime created) {
 
   public boolean solved() {
-    for (Guess guess : guesses) {
-      if (guess.solution()) {
-       return true;
-      }
-    }
-    return false;
+    //noinspection Convert2MethodRef // TODO Explore use of method reference.
+    return guesses
+        .stream()
+        .anyMatch((guess) -> guess.solution());
   }
 
 }
---
+++ services/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerService.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.codebreaker.service;
+
+public interface CodebreakerService {
+
+  // TODO: 6/15/26 Declare abstract methods to start game, get game, delete game, and submit guess. 
+  
+}
