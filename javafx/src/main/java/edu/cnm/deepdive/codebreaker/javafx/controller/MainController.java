03dab0d8ce087a8ff7f49ee1cd0de82ba3530439
jazz <jscott85@student.cnm.edu>
2026-06-24 10:46:29 -0600

    Module definition in progress.

--- javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/controller/MainController.java
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/controller/MainController.java
@@ -1,5 +1,9 @@
 package edu.cnm.deepdive.codebreaker.javafx.controller;
 
+import edu.cnm.deepdive.codebreaker.model.Guess;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,55 +13,63 @@ import javafx.scene.control.TextField;
 
 public class MainController {
 
- @FXML
- private Button settings;
-
- @FXML
-private ListView guesses; // FIXME: 6/23/26 Add the type parameter for Guess.
-
- @FXML
-  private Button submitGuess;
+  private static final String PROPS_FILE = "properties/code.properties";
+  private static final String POOL_KEY = "pool";
+  private static final String LENGTH_KEY = "length";
 
   @FXML
- private TextField guessInput;
-
+  private Button settings;
+  @FXML
+  private ListView<Guess> guesses;
+  @FXML
+  private Button submitGuess;
+  @FXML
+  private TextField guessInput;
   @FXML
   private Button startGame;
-
   @FXML
   private ResourceBundle resources;
 
+  private String pool;
+  private int length;
+
   public void shutdown() {
-   // viewModel.shutdown();
+    // viewModel.shutdown();
   }
 
   @FXML
-  void initialize() {
-   submitGuess.setDisable(true);
-   // TODO Pass a CellFactory to the guesses listview.
-   //TODO Register an observer (consumer0 of Game with the viewmodel:
-   //     If game is not null:
-   //     1. Clear out the observableList of guesses that the listview is holding.
-   //     2. Add all of the guesses in the updated game to the listviews observable.
-   //TODO Register an observer of Throwable (error) with the viewmodel:
-   //   If error is not null:
-   //     1. Display a message to the user, indicating the error.(probably be a network connection/resolution/timeout error)
+  void initialize() throws IOException {
+    try (InputStream input = getClass().getClassLoader().getResourceAsStream(PROPS_FILE)) {
+      Properties properties = new Properties();
+      properties.load(input);
+      pool = properties.getProperty(POOL_KEY);
+      length = Integer.parseInt(properties.getProperty(LENGTH_KEY));
+    }
+    submitGuess.setDisable(true);
+    // TODO Pass a CellFactory to the guesses listview.
+    //TODO Register an observer (consumer0 of Game with the viewmodel:
+    //     If game is not null:
+    //     1. Clear out the observableList of guesses that the listview is holding.
+    //     2. Add all of the guesses in the updated game to the listviews observable.
+    //TODO Register an observer of Throwable (error) with the viewmodel:
+    //   If error is not null:
+    //     1. Display a message to the user, indicating the error.(probably be a network connection/resolution/timeout error)
   }
 
   @FXML
-   void startGame(ActionEvent actionEvent) {
+  void startGame(ActionEvent actionEvent) {
     // TODO: 6/23/26 Use the viewmodel to start the game.
     //viewModel.startGame("ABCDEF", 3);
   }
 
   @FXML
-   void submitGuess(ActionEvent actionEvent) {
+  void submitGuess(ActionEvent actionEvent) {
     // TODO: 6/23/26 Submit guess for current game.
-   // viewModel.submitGues(...)
+    // viewModel.submitGues(...)
   }
 
   @FXML
   void showSettings(ActionEvent actionEvent) {
-   // TODO: 6/23/26 Open settings window.
- }
+    // TODO: 6/23/26 Open settings window.
+  }
 }
