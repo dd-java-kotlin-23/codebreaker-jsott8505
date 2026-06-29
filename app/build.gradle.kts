f886c642c735fa2e78829fe4509b4dfc3d89614c
jazz <jscott85@student.cnm.edu>
2026-06-29 15:46:25 -0600

    Starting build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.com.amazonaws.PredefinedClientConfigurations.defaultConfig
+
 plugins {
     alias(libs.plugins.android.application)
  //   alias(libs.plugins.hilt)
@@ -9,6 +11,24 @@ android {
     namespace = projects.property("basePackageName") as String
     compileSdk = (project.property("targetSdk")as String).toInt()
 
+    defaultConfig{
+
+        applicationId = project.property("basePackageName") as String
+        minSdk = (project.property("targetSdk")as String).toInt()
+        targetSdk = (project.property("targetSdk")as String).toInt()
+        versionCode = (project.property("versionCode") as String).toInt()
+        versionName = project.property("version") as String
+
+        resValue("string", "app_name", project.property("appName") as String)
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            // TODO: Add ProGuard.
+        }
+    }
 }
 
 dependencies{
