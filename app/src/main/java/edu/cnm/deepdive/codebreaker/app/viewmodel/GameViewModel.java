d0d4970a5251c116c976b626a788b0cd82f2c2e7
jazz <jscott85@student.cnm.edu>
2026-06-30 11:02:10 -0600

    Building a rudimentary layout

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/viewmodel/GameViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/viewmodel/GameViewModel.java
@@ -4,12 +4,12 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 import dagger.hilt.android.lifecycle.HiltViewModel;
 import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.service.CodebreakerService;
 import jakarta.inject.Inject;
-import java.util.function.Function;
 
 @HiltViewModel
 public class GameViewModel extends ViewModel {
@@ -18,17 +18,23 @@ public class GameViewModel extends ViewModel {
 
   private final CodebreakerService service;
   private final MutableLiveData<Game> game;
+  private final LiveData<Boolean> solved;
   private final MutableLiveData<Throwable> error;
 
   @Inject
   public GameViewModel(CodebreakerService service) {
     this.service = service;
     game = new MutableLiveData<>();
+    solved = Transformations.map(game, Game::isSolved);
     error = new MutableLiveData<>();
   }
 
   public LiveData<Game> getGame() {
     return game;
+  }
+
+  public LiveData<Boolean> getSolved() {
+    return Transformations.distinctUntilChanged(solved);
   }
 
   public LiveData<Throwable> getError() {
@@ -41,6 +47,14 @@ public class GameViewModel extends ViewModel {
         .startGame("ABCDEF", 3)
         .thenAccept(game::postValue)
         .exceptionally(this::postError);
+  }
+
+  public void submitGuess(String text) {
+  error.setValue(null);
+  service
+      .submitGuess(game.getValue(), text)
+      .thenAccept(game::postValue)
+      .exceptionally(this::postError);
   }
 
   @Nullable
